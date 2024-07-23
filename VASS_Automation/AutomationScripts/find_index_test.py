4faa0b0637c4f83a4dba23fbee1b81672336db6c
DrYangZ <68727867+DrYangZ@users.noreply.github.com>
2024-07-23 21:41:14 +0800

    Delete VASS_Automation directory

--- VASS_Automation/AutomationScripts/find_index_test.py
+++
@@ -1,6 +0,0 @@
-import re
-
-text = "哈哈哈(你是谁)（我是谁）"
-pattern = r'[\(\（][^()（）]*[\)\）]'
-result = re.sub(pattern, '', text)
-print(result)
--- VASS_Automation/RegistrationFromAll_run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd .\AutomationScripts\
-C:\Users\ALGU1VY\Desktop\python_pip\python.exe RegistrationFromAll.py
-pause
